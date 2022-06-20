add7a3baa5b47c04d489837ae65dbce82ef09ad9
hugosinp <hugo.anaponn@gmail.com>
2022-06-20 09:51:54 +0200

    added MongoDB imports

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Message, MessageSchema } from './schema/messages.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Message.name,
+        useFactory: () => {
+          const schema = MessageSchema;
+          return schema;
+        },
+      },
+    ]),
+  ],
   controllers: [MessagesController],
   providers: [MessagesService]
 })
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Post, PostSchema } from './schema/posts.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Post.name,
+        useFactory: () => {
+          const schema = PostSchema;
+          return schema;
+        },
+      },
+    ]),
+  ],
   controllers: [PostsController],
   providers: [PostsService]
 })
